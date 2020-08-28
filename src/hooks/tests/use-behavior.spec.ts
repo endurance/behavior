8ec7a3b16371b92f06c8cbc76cb30c36854dd8d5
Endurance Idehen <endurance.idehen@rigup.co>
2020-08-28 17:20:10 -0500

    behavior

--- src/hooks/tests/use-behavior.spec.ts
+++ src/hooks/tests/use-behavior.spec.ts
@@ -1,9 +1,6 @@
 import {renderHook, act} from '@testing-library/react-hooks'
-import { BaseBehavior } from '../../base-behavior';
 import { useSimpleBehavior } from '../simple-behavior';
 import { AppViewState } from '../../helper-types';
-
-// class Behavior extends BaseBehavior<any> {}
 
 describe('Test behavior hooks', () => {
   it('should be able to render and check hook', () => {
